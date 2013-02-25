2d0a04b47431acd9fc0e0577e180ccf8d267817b
Benoît Bryon <benoit@marmelune.net>
2013-02-25 12:19:40 +0100

    If secondary branch is not explicitely provided, use current branch. I.e. by default, psykorebase current branch on top of primary branch. Allows 'psykorebase master'.

--- psykorebase/cli.py
+++ psykorebase/cli.py
@@ -12,13 +12,19 @@ class ExecutionError(Exception):
     """Raised when some subprocess returns error."""
 
 
-def execute(command, stdin=None, stdout=None, stderr=None):
-    answer = ''
-    while answer not in ['y', 'n']:
-        answer = raw_input('About to execute "%s". Continue? [y, n] '
-                           % command)
-    if answer is 'n':
-        raise ExecutionError('ABORTED BY USER')
+def execute(command, stdin=None, stdout=None, stderr=None, confirm=False,
+            capture=False):
+    if confirm:
+        answer = ''
+        while answer not in ['y', 'n']:
+            answer = raw_input('About to execute "%s". Continue? [y, n] '
+                               % command)
+        if answer is 'n':
+            raise ExecutionError('ABORTED BY USER')
+    if capture and stdout is None:
+        stdout = subprocess.PIPE
+    if capture and stderr is None:
+        stderr = subprocess.PIPE
     process = subprocess.Popen(command, stdin=stdin, stdout=stdout,
                                stderr=stderr)
     retcode = process.wait()
@@ -40,12 +46,18 @@ def cd(path):
 
 class GitRebaser(object):
     """Git implementation of psykorebase."""
-    def __init__(self, repository_path, primary_branch, secondary_branch):
+    def __init__(self, repository_path, primary_branch, secondary_branch=None):
         self.repository_path = repository_path
         self.primary_branch = primary_branch
+        if secondary_branch is None:
+            secondary_branch = self.current_branch()
         self.secondary_branch = secondary_branch
         self.target_branch = '%s-rebased-on-top-of-%s' % (secondary_branch,
                                                           primary_branch)
+        if self.secondary_branch == self.primary_branch:
+            raise NotImplementedError('Unable to psykorebase {branch} on '
+                                      'itself'.format(
+                                          branch=self.primary_branch))
 
     def rebase(self):
         """Actually rebase."""
@@ -62,24 +74,41 @@ class GitRebaser(object):
             command = ['git', 'checkout', branch]
             if create:
                 command.insert(2, '-b')
-            execute(command)
+            execute(command, confirm=True)
 
     def merge(self, branch, message=None):
         with cd(self.repository_path):
             command = ['git', 'merge', branch]
             if message is not None:
                 command.extend(['-m', message])
-            execute(command)
+            execute(command, confirm=True)
 
     def delete_branch(self, branch):
         with cd(self.repository_path):
             command = ['git', 'branch', '-d', branch]
-            execute(command)
+            execute(command, confirm=True)
 
     def rename_branch(self, src, dst):
         with cd(self.repository_path):
             command = ['git', 'branch', '-m', src, dst]
-            execute(command)
+            execute(command, confirm=True)
+
+    def current_branch(self):
+        """Return name of current branch, using ``git branch``."""
+        command = ['git', 'branch']
+        process = execute(command, capture=True)
+        stdout, __ = process.communicate()
+        branches = stdout.split('\n')
+        # Current branch is the one highlighted with an asterisk.
+        current_branch = None
+        for branch in branches:
+            if branch.startswith('*'):
+                current_branch = branch[2:]
+                break
+        if current_branch is None:
+            raise ExecutionError('Unable to guess current branch with "git '
+                                 'branch".')
+        return current_branch
 
 
 def is_git(path):
@@ -118,17 +147,18 @@ def psykorebase(args=sys.argv[1:]):
     parser.add_argument('secondary_branch',
                         metavar='SECONDARY-BRANCH',
                         type=str,
-                        nargs=1,
+                        nargs='?',
+                        default=None,
                         help='Branch with changes you want to rebase.')
     parsed_args = parser.parse_args(args)
     dvcs_type = get_dvcs_type(repository_path)
     primary_branch = parsed_args.primary_branch[0]
-    secondary_branch = parsed_args.secondary_branch[0]
-    print "Working with %s, rebasing %s on top of %s" % (dvcs_type,
-                                                         secondary_branch,
-                                                         primary_branch)
+    secondary_branch = parsed_args.secondary_branch
     factory = get_rebaser_factory(dvcs_type)
     rebaser = factory(repository_path,
                       primary_branch,
                       secondary_branch)
+    print "Working with {dvcs}, rebasing {secondary} on top of {primary}" \
+          .format(dvcs=dvcs_type, primary=rebaser.primary_branch,
+                  secondary=rebaser.secondary_branch)
     rebaser.rebase()
